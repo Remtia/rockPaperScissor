2bcd7afd64a0729b19ff97ad7300c1f15b3e8ec2
Remtia <224189853+Remtia@users.noreply.github.com>
2025-12-13 23:57:25 +0000

    Logic added

--- app.js
+++ app.js
@@ -1,79 +1,65 @@
-const choicesArr = document.querySelector('.choices-card');
-const humanRockChoice = document.querySelector('#rock-human');
-const humanPaperChoice = document.querySelector('#paper-human');
-const humanScissorChoice = document.querySelector('#scissors-human');
+const humanRockChoice = document.querySelector("#rock-human");
+const humanPaperChoice = document.querySelector("#paper-human");
+const humanScissorsChoice = document.querySelector("#scissors-human");
 const computerRockChoice = document.querySelector('#rock-computer');
 const computerPaperChoice = document.querySelector('#paper-computer');
-const computerScissorChoice = document.querySelector('#scissors-computer');
-
-let humanScore = 0;
-let computerScore = 0;
-
-
-function getHumanChoice() {
-  if(humanRockChoice.addEventListener('click', function(e) {
-    e.target.style.transform = 'scale(1.5)'
-  })) {
-    return "rock";
-  }
-  else if(humanPaperChoice.addEventListener('click', function(e) {
-    e.target.style.transform = 'scale(1.5)'
-  })) {
-    return "paper";
-  }
-  else if (humanScissorChoice.addEventListener('click', function(e) {
-    e.target.style.transform = 'scale(1.5)'
-  })) {
-    return "scissors"
-  }
-}
+const computerScissorsChoice = document.querySelector('#scissors-computer');
+const humanScore = document.querySelector('.human-score');
+const computerScore = document.querySelector('.computer-score');
+const resultAnnouncement = document.querySelector('.result')
+const humanChoices = document.querySelectorAll('.human-choice');
+const computerChoices = document.querySelectorAll('.computer-choice');
 
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3) + 1;
   if (randomNum === 1) {
-    computerRockChoice.style.transform   = 'scale(1.5)'
+    computerRockChoice.style.transform = 'scale(1.3)';
     return "rock";
   } else if (randomNum === 2) {
-    computerRockChoice.style.transform   = 'scale(1.5)'
+    computerPaperChoice.style.transform = 'scale(1.3)';
     return "paper";
   } else {
-    computerRockChoice.style.transform   = 'scale(1.5)';
-     return "scissor";
+    computerScissorsChoice.style.transform = 'scale(1.3)';
+    return "scissors";
   }
 }
 
+humanRockChoice.addEventListener("click", (e) => {
+  handleHumanChoice("rock", e.currentTarget);
+});
+humanPaperChoice.addEventListener("click", (e) => {
+  handleHumanChoice("paper", e.currentTarget);
+});
+humanScissorsChoice.addEventListener("click", (e) => {
+  handleHumanChoice("scissors", e.currentTarget);
+});
 
-function playRound(humanSelection, computerSelection) {
-  if (humanSelection === computerSelection) {
-    return console.log(
-      `It's a tie, you chose ${humanSelection}, and computer also chose ${computerSelection} .`
-    );
+function resetScale() {
+  humanChoices.forEach(one => {
+    one.style.transform = 'scale(1)'
+  });
+  computerChoices.forEach(one => {
+    one.style.transform = 'scale(1)'
+  });
+}
+
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    resultAnnouncement.innerText = "This round is tie.";
   } else if (
-    (humanSelection === "rock" && computerSelection === "scissor") ||
-    (humanSelection === "scissor" && computerSelection === "paper")
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
   ) {
-    humanScore++;
-    return console.log(
-      `You won! You chose ${humanSelection}, and computer chose ${computerSelection}, the score is ${humanScore}.`
-    );
-  } else if (humanSelection === "paper" && computerSelection === "rock") {
-    humanScore++;
-    return console.log(
-      `You won! You chose ${humanSelection}, and computer chose ${computerSelection}.`
-    );
-  } else {
-    computerScore++;
-    return console.log(
-      `You lost. You chose ${humanSelection}, and computer chose ${computerSelection}.`
-    );
-  }
+    resultAnnouncement.innerText = "You won this round!";
+  } else resultAnnouncement.innerText = "You lost this round :(";
 }
 
-function playGame() {
-    let computerSelection = getComputerChoice();
-    let humanSelection = getHumanChoice();
-    playRound(humanSelection, computerSelection);
-    console.log(`The score is ${humanScore} to ${computerScore}`);
+function handleHumanChoice(choice, humanCard) {
+  resetScale();
+  humanCard.style.transform = "scale(1.3)";
+  const computerChoice = getComputerChoice();
+  playRound(choice, computerChoice);
 }
 
-playGame();
